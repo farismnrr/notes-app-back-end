b04a26c364fd411fc37878f62dc4423fee47f1eb
farismnrr <farismunir2@gmail.com>
2024-07-22 02:31:04 +0700

    Update to version 3.0

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
 const { nanoid } = require("nanoid");
 const InvariantError = require("../../exceptions/InvariantError");
+const { mapDBToModel } = require("../../utils");
 
 class NotesService {
 	constructor() {
