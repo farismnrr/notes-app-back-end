b73a70571dd1280e1912f63099ca95aa692ee7cd
farismnrr <farismunir2@gmail.com>
2024-07-24 20:53:50 +0700

    Update some bug

---
+++ README.md
@@ -0,0 +1,30 @@
+# notes-app-back-end
+
+---
+
+## How to Run Program
+
+#### 1. Check `.env.example` for required environment variables.
+#### 2. Run on Development
+
+```bash
+npm run build:local
+```
+
+#### 3. Run on Production (Linux Server Only)
+
+```bash
+npm run build:server
+```
+
+#### 4. Run on Docker
+
+- Build Docker Image
+```bash
+npm run build:docker
+```
+
+- Run Docker Container
+```bash
+npm run start:docker
+```
---
+++ src/server.js
@@ -0,0 +1,66 @@
+const Hapi = require("@hapi/hapi");
+const dotenv = require("dotenv");
+const notes = require("./api/notes");
+const NotesService = require("./services/inMemory/NotesService");
+const NotesValidator = require("./validator/notes");
+const RequestService = require("./services/RequestService");
+const ClientError = require("./exceptions/ClientError");
+
+dotenv.config();
+
+const createServer = () => {
+	const server = Hapi.server({
+		port: process.env.PORT || 3000,
+		host: process.env.HOST || "localhost",
+		routes: {
+			cors: {
+				origin: ["*"]
+			}
+		}
+	});
+
+	return server;
+};
+
+const registerPlugins = async server => {
+	await server.register({
+		plugin: notes,
+		options: {
+			service: new NotesService(),
+			validator: NotesValidator
+		}
+	});
+};
+
+const setupRequestService = server => {
+	if (process.env.NODE_ENV !== "production") {
+		server.ext("onRequest", (request, h) => new RequestService().onRequest(request, h));
+		server.ext("onPreResponse", (request, h) => new RequestService().onPreResponse(request, h));
+	}
+};
+
+const handleClientError = server => {
+	server.ext("onPreResponse", (request, h) => {
+		const { response } = request;
+		if (response instanceof ClientError) {
+			const newResponse = h.response({
+				status: "fail",
+				message: response.message
+			});
+			newResponse.code(response.statusCode);
+			return newResponse;
+		}
+		return h.continue;
+	});
+};
+
+const startServer = async () => {
+	const server = createServer();
+	await registerPlugins(server);
+	setupRequestService(server);
+	handleClientError(server);
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+startServer();
