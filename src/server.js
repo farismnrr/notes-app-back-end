2c1f79a0c399f687527942d15b1954dcb1c47d0f
farismnrr <farismunir2@gmail.com>
2024-07-25 17:52:28 +0700

    add: user service

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const Hapi = require("@hapi/hapi");
 const dotenv = require("dotenv");
+
+// notes
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
+
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
 const RequestService = require("./services/RequestService");
 const ClientError = require("./exceptions/ClientError");
 
@@ -23,19 +31,29 @@ const createServer = () => {
 };
 
 const registerPlugins = async server => {
-	await server.register({
-		plugin: notes,
-		options: {
-			service: new NotesService(),
-			validator: NotesValidator
+	await server.register([
+		{
+			plugin: notes,
+			options: {
+				service: new NotesService(),
+				validator: NotesValidator
+			}
+		},
+		{
+			plugin: users,
+			options: {
+				service: new UsersService(),
+				validator: UsersValidator
+			}
 		}
-	});
+	]);
 };
 
 const setupRequestService = server => {
 	if (process.env.NODE_ENV !== "production") {
-		server.ext("onRequest", (request, h) => new RequestService().onRequest(request, h));
-		server.ext("onPreResponse", (request, h) => new RequestService().onPreResponse(request, h));
+		const requestService = new RequestService();
+		server.ext("onRequest", requestService.onRequest);
+		server.ext("onPreResponse", requestService.onPreResponse);
 	}
 };
 
